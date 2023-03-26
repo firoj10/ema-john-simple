842756f9a0a8e51f3b35c4596bdcc41a05b24b24
firoj10 <mdfirojhasan.info@gmail.com>
2023-03-27 01:09:18 +0600

    product added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
     console.log(props.product)
     const {img, name, seller, ratings, price} = props.product;
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
            <img src={img} alt="" />
@@ -14,7 +17,9 @@ const Product = (props) => {
            <p>Manufacturer:{ratings}</p>
            <p>Ratining:{price} Stars</p>
           </div>
-          <button className='btn-cart'>add to card</button>
+          <button onClick={()=>handleAddToCart(props.product)} className='btn-cart'>add to card
+          <FontAwesomeIcon icon={faShoppingCart}/>
+          </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,11 +4,16 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts]=useState([]);
+    const [cart, setCart]=useState([]);
     useEffect( ()=>{
         fetch('products.json')
         .then(res=> res.json())
         .then(data => setProducts(data))
     }, [])
+    const handleAddToCart = (product) =>{
+       const newCart = [...cart, product];
+       setCart(newCart);
+    }
     return (
      
         <div className='shop-container'>
@@ -17,11 +22,13 @@ const Shop = () => {
         products.map(product=> <Product
         product={product}
         key={product.id}
+        handleAddToCart={handleAddToCart  }
         ></Product>)
       }
             </div>
              <div className='card-container'>
-             <h1>header</h1>
+             <h1>ORDER SUMMARY</h1>
+        <p>Select items:{cart.length}</p>
             </div>
         </div>
     );
