a30b4b04ef649939b0d128756cadf6e9ad0032ba
firoj10 <mdfirojhasan.info@gmail.com>
2023-03-27 22:19:29 +0600

    added cart component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+// import '/.Card.css'
+import './Card.css'
+
+const Card = ({cart}) => {
+    // const {cart}= props;
+    // const cart= props.cart;
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for (const product of cart){
+        totalPrice = totalPrice+product.price;
+        totalShipping = totalShipping+product.shipping;
+    }
+   const  tax = totalPrice*7/100;
+   const grandTotal = totalPrice +  totalShipping + tax;
+    return (
+        <div className='cart'>
+        <h1>ORDER SUMMARY</h1>
+        <p>Select items:{cart.length}</p>
+        <p>Total Price: ${totalPrice}</p>
+        <p>Total Shapping: {totalShipping}</p>
+        <p>Tex:{tax.toFixed(2)}</p>
+        <h3>grandTotal:{grandTotal.toFixed(2)}</h3>
+        </div>
+    );
+};
+
+export default Card;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Card from '../Card/Card';
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -27,8 +28,7 @@ const Shop = () => {
       }
             </div>
              <div className='card-container'>
-             <h1>ORDER SUMMARY</h1>
-        <p>Select items:{cart.length}</p>
+        <Card cart={cart}></Card>
             </div>
         </div>
     );
